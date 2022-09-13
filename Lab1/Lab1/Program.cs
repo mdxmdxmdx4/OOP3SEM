067e6c4069e2568e475a3cdbb6639964fd814299
Dmitry <leshuk_03@mail.ru>
2022-09-13 21:37:53 +0300

    refactor + lab1 is completed

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Specialized;
+﻿using System;
+using System.Collections.Specialized;
 using System.Text;
 
 namespace Lab1
@@ -18,13 +19,13 @@ namespace Lab1
             short TaskA6 = -12314;
             int TaskA7 = 10;
             double TaskA8 = 7.1234123;
-            Console.WriteLine(".........1)Типы..........\n" +
-                " ----Task A----\n Bool = {0} \n Char = {1} \n Long = {2} \n Decimal = {3} \n" +
+            Console.WriteLine(".........1)Types..........\n" +
+                " \n----Task A----\n Bool = {0} \n Char = {1} \n Long = {2} \n Decimal = {3} \n" +
                 " Float = {4} \n Short = {5} \n Int = {6}\n Double = {7}\n ",
                 TaskA1, TaskA2, TaskA3, TaskA4, TaskA5, TaskA6, TaskA7, TaskA8);
             //task B
             int b1 = Convert.ToInt16(0.981231);  //1 явное
-            Console.WriteLine("----Task B---- \n b1 = " + b1 + " , " + b1.GetType());
+            Console.WriteLine(" ----Task B---- \n b1 = " + b1 + " , " + b1.GetType());
             long b2 = (long)b1; //2 явное
             short b3 = (short)b2; //3 явное
             bool b4 = Convert.ToBoolean(b3); //4 явное
@@ -47,29 +48,29 @@ namespace Lab1
             //task D
             var a = 28;
             var b = 1.123412;
-            Console.WriteLine("\n----Task D----\nvar a is " + a.GetType() + ", var b is" + b.GetType());
+            Console.WriteLine("\n ----Task D----\nvar a is " + a.GetType() + ", var b is" + b.GetType());
             var c = a + b;
             Console.WriteLine("var c is " + a.GetType());
             //task E
-            Console.WriteLine("\n----Task E----\n");
+            Console.WriteLine("\n ----Task E----\n");
             int? e1 = null;
             int? e2 = null;
             Console.WriteLine("Compairing nullable = {0}\nSum of nullable = {1}", e1 == e2, e1 + e2);
             //task F
-            Console.WriteLine("\n----Task F----\n");
+            Console.WriteLine("\n ----Task F----\n");
             var f1 = 123.12312;
             f1 = 12;
             Console.WriteLine("f1 = {0}, {1}", f1, f1.GetType());
             //2)  Строки
             //Task A
-            Console.WriteLine(".........2)Строки..........\n----Task A----\n");
+            Console.WriteLine("\n\n.........2)Strings..........\n\n ----Task A----\n");
             string s1 = "wex";
             string s2 = "wex";
             string s3 = "notwex";
             Console.WriteLine($"Compairing strings s1 и s2: {s1 == s2}");
             Console.WriteLine($"Compairing strings s2 и s3 : {s2 == s3}");
             //Task B
-            Console.WriteLine("\n----Task B----\n");
+            Console.WriteLine("\n ----Task B----\n");
             string string1 = "exort";
             string string2 = "some words here";
             string string3 = "amg";
@@ -83,13 +84,13 @@ namespace Lab1
                 Console.WriteLine(asd);
             }
             Console.WriteLine("Inserting string in string: " + string3.Insert(2, string1));
-            Console.WriteLine("Deleting substring: " + string2.Remove(6) + "\n ----Task C----");
+            Console.WriteLine("Deleting substring: " + string2.Remove(6) + "\n\n ----Task C----");
             //Task C
             string nllstr = null;
             string vstr = "";
             Console.WriteLine("isnullorempty on nullstr: {0} , on voidstr : {1}", string.IsNullOrEmpty(nllstr),
                 string.IsNullOrEmpty(vstr));
-            Console.WriteLine("Equality of strings : " + (nllstr == vstr) + ", Concantenation: " + String.Concat(vstr, nllstr) + "\n ----Task D----\n");
+            Console.WriteLine("Equality of strings : " + (nllstr == vstr) + ", Concantenation: " + String.Concat(vstr, nllstr) + "\n\n ----Task D----\n");
             //Task D
             StringBuilder SB = new StringBuilder("ABC", 20);
             SB.Remove(1, 2);
@@ -101,7 +102,7 @@ namespace Lab1
             int ArrayWidth = 4;
             int ArrayHeight = 5;
             int[,] DoubleArray = new int[ArrayWidth, ArrayHeight];
-            Console.WriteLine("\n\n.........3)Массивы..........\n----Task A----\n Matrix:)");
+            Console.WriteLine("\n\n.........3)Arrays..........\n\n ----Task A----\n Matrix:)");
             for (int j = 0; j < ArrayWidth; j++)
             {
                 for (int k = 0; k < ArrayHeight; k++)
@@ -113,7 +114,7 @@ namespace Lab1
             }
             //Task B
             string[] StringArr = { "scars", "up", "on", "my", "wrist" };
-            Console.WriteLine("----Task B----:\n");
+            Console.WriteLine("\n ----Task B----:\n");
             foreach (var elem in StringArr)
             {
                 Console.Write(elem + " | ");
@@ -128,7 +129,7 @@ namespace Lab1
                 Console.Write(elem + ",");
             }
             //Task C           !!!!!!!!!!!!!!!!!!!!!! ДОДЕЛАТЬ!!!!!!!!!!!!
-            Console.WriteLine("\n\n----Task C---");
+            Console.WriteLine("\n\n ----Task C---");
             float[][] StepArray = new float[3][];
             for (var ai = 1; ai < 4; ai++)
             {
@@ -155,16 +156,93 @@ namespace Lab1
             }
 
             //
-            Console.WriteLine("----Task D---");
+            Console.WriteLine("\n ----Task D---");
             var array1 = new object[0];
             var str = "";
             Console.WriteLine("1st type: {0} ,2nd type: {1}",array1.GetType(), str.GetType());
             // 4 Кортежи
-            /*
-                        (int, string, char, string, ulong) referens = (1, "HEad", 'g', "food", 65758758);
+            Console.WriteLine("\n\n.........4)Tuple..........\n ----Task A----");
+            
+                        (int, string, char, string, ulong) reference = (1, "HEad", 'g', "food", 65758758);
 
-                        Console.WriteLine(referens.Item1 + referens.Item2 + referens.Item2 + referens.Item3 + referens.Item4 + referens.Item5);
-                        Console.WriteLine("Vvedite nomera, kotory xotite vyvesti");*/
+                        Console.WriteLine("весь кортеж -  " + reference + " ");
+                        Console.WriteLine("1st item" + reference.Item1);
+                        Console.WriteLine("2nd item" + reference.Item3);
+                        Console.WriteLine("3rd itme" + reference.Item5);
+
+            Console.WriteLine(" ----Task B----");
+            int int1;
+            string string01, string02;
+            char char1;
+            ulong ulong1;
+            Console.WriteLine(" ----Task C----");
+            (_, string1, _, _, ulong1) = reference;
+            (int1 , _, char1, string2, _) = reference;
+            Console.WriteLine(" ----Task D----");
+            var tuple1 = (1.1231f, 2m, 3, "131", 'q');
+            var tuple2 = (1.1231f, 2m, 3, "131", 'q');
+            Console.WriteLine("Equality of tuples:" + (tuple1 == tuple2));
+            Console.WriteLine("\n\n.........5)Local function..........\n");
+            static (int, int, int, char) Localfunction(int[] numbers, string str1)
+            {
+
+                int min = int.MaxValue;
+                int max = int.MinValue;
+                int sum = 0;
+
+
+                foreach (int elem in numbers)
+                {
+                    sum += elem;
+                    if (elem < min)
+                    {
+                        min = elem;
+                    }
+
+                    if (elem > max)
+                    {
+                        max = elem;
+                    }
+                }
+
+                char letter = str1[0];
+
+                return (max, min, sum, letter);
+
+            };
+             var nums = new[]
+                { 991, 1241, 777, 11, 31};
+            var strforfunc = "miracle^";
+            Console.WriteLine(Localfunction(nums, strforfunc));
+            Console.WriteLine("\n\n..........Checked/unchecked..........\n");
+            static int localf2()
+            {
+                int mval = int.MaxValue;
+                unchecked
+                {
+                    Console.WriteLine("unchecked: " + (mval + 1));
+                };
+                return mval;
+            };
+            static int localf3()
+            {
+                int mval2 = int.MaxValue;
+                try
+                {
+                    checked
+                    {
+                        Console.WriteLine(mval2 + 1);
+                    }
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine(e.Message);  // output: Arithmetic operation resulted in an overflow.
+                }
+                return mval2;
+            };
+            localf2();
+            Console.WriteLine("if we will use \" checked \", compiler will throw an error, so using try/catch construction\n");
+            localf3();
 
         }
 
