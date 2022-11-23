6786e42605e3802a1af5555ae7ad962a5d88db74
Dmitry <leshuk_03@mail.ru>
2022-11-23 13:13:09 +0300

    modified lab8

--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Lab8
 {
@@ -30,6 +31,56 @@ namespace Lab8
             Console.WriteLine(SQL);
 
 
+            Func<string, string> func;
+            Action<string> test2;
+            Func<string, string> func2;
+            Func<string, string> func3;
+            Func<string, string> func4;
+            func = str1 => {  //блочное лямбда-выражение(упрощенная запись анонимных методов) 
+                char[] sign = { '.', ',', '!', '?', '-', ':', '+', '=' };
+                for (int i = 0; i < str1.Length; i++)
+                {
+                    if (sign.Contains(str1[i]))
+                    {
+                        str1 = str1.Remove(i, 1);
+                    }
+                }
+                Console.WriteLine(str1);
+                return str1;
+            };
+            test2 = delegate (string str2)   //анонимный метод
+            {
+                str2 += " DelegateAction";
+                Console.WriteLine(str2);
+            };
+            func2 = str3 =>
+            {
+                str3 = str3.Replace(" ", "");
+                Console.WriteLine(str3);
+                return str3;
+            };
+            func3 = str4 =>
+            {
+                str4 = str4.ToUpper();
+                Console.WriteLine(str4);
+                return str4;
+            };
+            func4 = str5 =>
+            {
+                str5 = str5.ToLower();
+                Console.WriteLine(str5);
+                return str5;
+            };
+
+            string str = "one + five = seven";
+            Console.WriteLine("Строка в начале: " + str);
+            Console.WriteLine("Преобразование: ");
+            string s1, s2, s3;
+            s1 = ForStrings.RemoveS(str, func);
+            ForStrings.AddToString(s1, test2);
+            s2 = ForStrings.RemoveSpaces(s1, func2);
+            s3 = ForStrings.Upper(s2, func3);
+            ForStrings.Lower(s3, func4);
 
 
         }
